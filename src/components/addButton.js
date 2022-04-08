33c8d26ab81b9db9443b6916f2afd9a0c0a74d79
cmmille <cmmiller.bio@gmail.com>
2022-04-07 22:52:37 -0600

    Added Material UI support for aesthetics.

--- src/components/addButton.js
+++ src/components/addButton.js
@@ -1,9 +1,17 @@
-import "./addButton.css"
+import "./addButton.css";
+import Zoom from "@mui/material/Zoom";
+import Fab from "@mui/material/Fab";
 
-function AddButton(){
-    return <div className="right-container">
-        <button className="btn">+</button>
+function AddButton(props) {
+  return (
+    <div className="right-container">
+      <Zoom in={props.zoom}>
+        <Fab className="btn">
+          +
+        </Fab>
+      </Zoom>
     </div>
+  );
 }
 
 export default AddButton;
